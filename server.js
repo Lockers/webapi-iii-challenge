532abd8575e0f5286869098eb70c4690ce217c8b
Matt Locklin <mattlocklin@live.co.uk>
2019-07-10 15:48:50 +0100

    Nearly finished MVP just debugging last post

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
-const express = 'express';
+const express = require('express');
+const userRouter = require('./users/userRouter.js')
 
 const server = express();
+
+server.use(express.json())
+server.use('/', userRouter);
+server.use(logger)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
-//custom middleware
+// custom middleware
 
 function logger(req, res, next) {
-
+  console.log(`Request Method: ${req.method} Request URL: ${req.url} ${Date()}`)
+  next()
 };
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,108 @@
-const express = 'express';
-
+const express = require('express');
+const db = require('./userDb');
 const router = express.Router();
+const postDb = require('../posts/postDb')
 
-router.post('/', (req, res) => {
+router.post('/users', validateUser, (req, res) => {
+    res.status(200).json(req.user)
+});
+
+router.post('/users/:id/posts', validatePost, (req, res) => {
+    res.status(200).json(req.post)
+});
+
+router.get('/users', (req, res) => {
+    db.get()
+        .then(response => {
+            res.status(200).json(response)
+        })
+        .catch(error => {
+            res.status(500).json({Error: 'Internal server error'})
+        })
+});
+
+router.get('/users/:id', validateUserId, (req, res) => {
+    res.status(200).json(req.user)
+});
+
+router.get('/users/:id/posts', validateUserId, (req, res) => {
 
 });
 
-router.post('/:id/posts', (req, res) => {
-
+router.delete('/users/:id', validateUserId, (req, res) => {
+    res.status(204)
 });
 
-router.get('/', (req, res) => {
-
-});
-
-router.get('/:id', (req, res) => {
-
-});
-
-router.get('/:id/posts', (req, res) => {
-
-});
-
-router.delete('/:id', (req, res) => {
-
-});
-
-router.put('/:id', (req, res) => {
+router.put('/users/:id', validateUserId, (req, res) => {
 
 });
 
 //custom middleware
 
-function validateUserId(req, res, next) {
-
+async function validateUserId(req, res, next) {
+    try {
+        if (!isNaN(req.params.id)) {
+    
+            user = await db.getById(req.params.id)
+            
+            if (user !== undefined) {
+                req.user = user
+                next()
+            }
+            else {
+                res.status(404).json({ Err: 'ID not here babycakes'})
+            }
+        }
+        else {
+            res.status(400).json({ Mesage: 'User ID is not a number' })
+        }
+    } catch (error) {
+        console.log(error)
+    }
 };
 
-function validateUser(req, res, next) {
-
+async function validateUser(req, res, next) {
+    try {
+        
+        if (Object.keys(req.body) != 0) {
+            if (req.body.name) {
+                user = await db.insert(req.body)
+                req.user = user
+                next()
+            }
+            else {
+                res.status(400).json({ Message: 'Missing Required Name Field' })
+            }
+        }
+        else
+        {
+            res.status(400).json({ Message: 'Missing User Data' })
+        }
+    } catch (error) {
+        console.log(error)
+    }
 };
 
-function validatePost(req, res, next) {
+async function validatePost(req, res, next) {
+    try {
 
+        if (Object.keys(req.body) != 0) {
+            if (req.body.text) {
+                post = await postDb.insert(req.body)
+                req.post = post
+                next()
+            }
+            else {
+                res.status(400).json({ Message: 'Missing Required Name Field' })
+            }
+        }
+        else {
+            res.status(400).json({ Message: 'Missing User Data' })
+        }
+    } catch (error) {
+        console.log(error)
+    }
+    
 };
 
 module.exports = router;
